dd7a86a99c51c98cbfe5c8423e976daafcb34e3e
MarkiianDubyna <dybuna.mark@gmail.com>
2021-07-10 00:47:11 +0300

    homework 8

--- nodes-works/homework/constants/constant.js
+++ nodes-works/homework/constants/constant.js
@@ -3,8 +3,8 @@ module.exports = {
   DB_CONNECTION_URL: process.env.DB_CONECTION_URL || 'mongodb://localhost:27017/users',
   ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET || 'Secret',
   REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET || 'Refresh_Secret',
-  SYSTEM_EMAIL: process.env.SYSTEM_EMAIL || 'dybuna.mark@gmail.com',
-  SYSTEM_EMAIL_PASSWORD: process.env.SYSTEM_EMAIL_PASSWORD || 'Marik_prorok1',
+  SYSTEM_EMAIL: process.env.SYSTEM_EMAIL || 'example@gmail.com',
+  SYSTEM_EMAIL_PASSWORD: process.env.SYSTEM_EMAIL_PASSWORD || '12345',
 
   AUTHORIZATION: 'Authorization'
 };
